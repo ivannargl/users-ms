4784271fdc901c8470a579979125ce523be1aeec
UrielSC24 <2022371107@uteq.edu.mx>
2025-12-01 05:45:23 -0600

    Obtener usuarios de una universidad

--- src/main/java/mx/edu/uteq/idgs12/users_ms/controller/UserController.java
+++ src/main/java/mx/edu/uteq/idgs12/users_ms/controller/UserController.java
@@ -1,6 +1,7 @@
 package mx.edu.uteq.idgs12.users_ms.controller;
 
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import mx.edu.uteq.idgs12.users_ms.dto.ChangePasswordDTO;
+import mx.edu.uteq.idgs12.users_ms.dto.UserResponseDTO;
 import mx.edu.uteq.idgs12.users_ms.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,6 +22,16 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    /** Obtener todos los usuarios de una universidad */
+    @GetMapping("/university/{idUniversity}")
+    public ResponseEntity<List<UserResponseDTO>> getUsersByUniversity(
+            @PathVariable Integer idUniversity,
+            @RequestParam(required = false) Boolean active
+    ) {
+        List<UserResponseDTO> users = userService.getUsersByUniversity(idUniversity, active);
+        return ResponseEntity.ok(users);
+    }
 
     @PutMapping("/{id}/change-password")
     public ResponseEntity<?> changePassword(@PathVariable Integer id, @RequestBody ChangePasswordDTO dto) {
